6552c387987132090157772cf4e2e3251ae758d6
badbad <1946408873@qq.com>
2019-08-31 13:04:45 +0800

    java工程的第一次提交

---
+++ git_test/src/cn/itcast/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+/**
+ * @Author: badbad
+ * @Date: 2019/8/31 13:02
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git的java idea提交");
+    }
+}
